4afd3b8884063123e352525da02cfcc6bc32a47f
Mohmed <100777706+Mohmed932@users.noreply.github.com>
2023-12-15 14:21:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
     while (true) {
       await Alqaheranews(browser);
       const page = await browser.newPage();
-      await page.goto("https://alqaheranews.onrender.com", {
+      await page.goto("https://alqaheranews-ffjy.onrender.com", {
         waitUntil: "domcontentloaded",
         waitUntil: "load",
       });
