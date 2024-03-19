72d75e31a510c08808f37e562e03190751b21018
Mohmed932 <mohmedmahmoudfouad@gmail.com>
2024-03-19 17:33:23 +0200

    edit to lancher

--- index.js
+++ index.js
@@ -5,26 +5,19 @@ import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 import * as dotenv from "dotenv";
-import { Alqaheranews } from "./Scraping/Alqaheranews.js";
-
 dotenv.config();
+import { Alqaheranews } from "./Scraping/Alqaheranews.js";
 
 const uri = process.env.MONGODB_URL;
 const port = process.env.PORT || "5000";
 
 const app = express();
 
-// middleware
+// midelware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(
-  cors({
-    origin: "https://awalbawl.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use(compression());
 
 // routes
@@ -32,55 +25,54 @@ app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
 });
 
-// puppeteer scraping route
-app.get("/scrape", async (req, res) => {
-  let browser = null;
+// puppeteer scraping
+(async () => {
   try {
-    browser = await puppeteer.launch({
-      headless: process.env.NODE_ENV === "production",
+    const browser = await puppeteer.launch({
       args: [
         "--disable-setuid-sandbox",
         "--no-sandbox",
         "--single-process",
         "--no-zygote",
       ],
+      headless: "new",
       executablePath:
         process.env.NODE_ENV === "production"
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
     });
 
-    await Alqaheranews(browser);
-
     const page = await browser.newPage();
-    await page.goto("https://alqaheranews-j39n.onrender.com", {
+    await page.goto("https://sadaelbalad-quj0.onrender.com", {
       waitUntil: "domcontentloaded",
     });
-    await page.close();
 
-    res.send("Scraping completed!");
-  } catch (error) {
-    console.error("Error during scraping:", error);
-    res.status(500).send("Internal Server Error");
-  } finally {
-    if (browser) {
-      await browser.close();
+    while (true) {
+      await Alqaheranews(browser);
+
+      // Close the current page and open a new one
+      await page.close();
+      const newPage = await browser.newPage();
+      await page.goto("https://alqaheranews-j39n.onrender.com", {
+        waitUntil: "domcontentloaded",
+      });
+      page = newPage; // Update the page reference to the new page
     }
+  } catch (error) {
+    if (error) throw error;
+    console.error("Error in Puppeteer scraping:", error);
   }
-});
+})();
 
 app.use("*", (req, res) => {
-  res.status(404).json("Page not found");
+  res.status(404).json("this page not found");
 });
 
-// connect to the database then listen to the server
+// conect to database then listen to server
 connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to the database");
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
-  });
+  .then(() =>
+    app.listen(port, async (req, res) => {
+      console.log("conected");
+    })
+  )
+  .catch(() => console.log("not conected"));
