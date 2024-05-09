ae8e3ea16f8be075abd53b4fb950813fbd71a095
Mohmed <100777706+Mohmed932@users.noreply.github.com>
2024-05-09 17:11:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
     while(true){
       await Alqaheranews(browser)
       const page = await browser.newPage()
-      await page.goto("https://alqaheranews-52i8.onrender.com",{
+      await page.goto("https://alqaheranews-zsao.onrender.com",{
         waitUntil: "domcontentloaded",
         waitUntil: "load",
       })
