b0dcdbca99f9bc63be8bf24a22f68edcec9593e6
Mohmed <100777706+Mohmed932@users.noreply.github.com>
2025-02-17 20:27:31 +0200

    Update OpenBrowser.js

--- utiles/OpenBrowser.js
+++ utiles/OpenBrowser.js
@@ -3,23 +3,17 @@ import puppeteer from "puppeteer";
 export const OpenBrowser = async () => {
   try {
     const browser = await puppeteer.launch({
-      // headless: process.env.NODE_ENV === "production",
+      headless: true, 
+      executablePath: "/usr/bin/chromium-browser", 
       args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-        "--start-maximized",
-        "--disable-infobars",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? '/usr/bin/chromium-browser'
-          : puppeteer.executablePath(),
+        
+        "--no-sandbox", 
+        "--disable-gpu", 
+      ], 
     });
     return browser;
   } catch (error) {
     console.error("❌ فشل فتح المتصفح:", error);
-    return null; // إرجاع null بدلاً من إيقاف التطبيق
+    return null;
   }
 };
