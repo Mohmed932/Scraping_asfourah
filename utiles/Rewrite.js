4c91f75237a8fbb86c0897961711b8899b45302b
Mohmed932 <mohmedmahmoudfouad@gmail.com>
2025-02-23 16:03:51 +0200

    remove stars from text

--- utiles/Rewrite.js
+++ utiles/Rewrite.js
@@ -1,12 +1,17 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const removeStarsFromText = (text) => {
+  return text.replace(/\*\*(.*?)\*\*/g, "$1");
+};
+
 export const rewriteScence = async (scence, key) => {
   try {
     const genAI = new GoogleGenerativeAI(key);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = `اكتب هذا النص بطريقة مختصرة وبأسلوب جذاب: ${scence}`;
     const result = await model.generateContent(prompt);
-    return result.response.text();
+    const text = removeStarsFromText(result.response.text());
+    return text;
   } catch (error) {
     console.error("Error Message: ", error);
     return scence;
